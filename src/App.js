a2756666135efa4c9c8c5d6b3d81114ad37beff9
bredha <bredha@stc.com.sa>
2024-09-22 15:41:41 +0100

    adding pages and routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-// function App() {
-// 	return <h1>Hello world</h1>;
-// }
-
-// export default App;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/forgotPassword" element={<ForgotPassword />} />
+				<Route path="/offers" element={<Offers />} />
+				<Route path="/signIn" element={<SignIn />} />
+				<Route path="/signUp" element={<SignUp />} />
+			</Routes>
+		</Router>
+	);
 }
