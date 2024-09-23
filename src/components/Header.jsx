10bf33375b7403499cf57cb579262211a91208f0
bredha <bredha@stc.com.sa>
2024-09-23 19:10:56 +0100

    create sign in ,sign up , forgot password , and OAuth Component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -41,7 +41,7 @@ export default function Header() {
 						</li>
 						<li
 							className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-								pathMatchRoute("/sign-in") && "text-black border-b-red-500"
+								pathMatchRoute("/") && "text-black border-b-red-500"
 							}`}
 							onClick={() => navigate("/sign-in")}>
 							Sign in
