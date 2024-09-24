6717be4bb9d0dd1e91480d592e06cc274e73f4ea
bredha <bredha@stc.com.sa>
2024-09-24 14:50:52 +0100

    complete the sign in functionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,20 +1,38 @@
 import React, { useState } from "react";
 import { FaEye } from "react-icons/fa";
 import { FaEyeSlash } from "react-icons/fa6";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import OAuth from "../components/OAuth";
+import { signInWithEmailAndPassword, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
 
 export default function SignIn() {
 	const [showPassword, setShowPassword] = useState(false);
 
 	const [formData, setFormData] = useState({ email: "", password: "" });
 	const { email, password } = formData;
-
+	const navigate = useNavigate();
 	function onChange(e) {
 		setFormData((prevState) => ({
 			...prevState,
 			[e.target.id]: e.target.value,
 		}));
+	}
+	async function onSubmit(e) {
+		e.preventDefault();
+		try {
+			const auth = getAuth();
+			const userCredential = await signInWithEmailAndPassword(
+				auth,
+				email,
+				password
+			);
+			if (userCredential.user) {
+				navigate("/");
+			}
+		} catch (error) {
+			toast.error = "Bad user credentials";
+		}
 	}
 	return (
 		<section>
@@ -29,7 +47,7 @@ export default function SignIn() {
 					/>
 				</div>
 				<div className="w-full md:w-[67%] lg:w-[40%] lg:ml-20">
-					<form>
+					<form onSubmit={onSubmit}>
 						<input
 							className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white border-gray-300 rounded transition ease-in-out"
 							type="email"
